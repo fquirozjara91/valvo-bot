68e54ea5c8cec3577efeae39c16500fc75335a19
Felipe Quiroz Jara <felipe.quirozj@hotmail.com>
2020-01-15 12:54:27 -0300

    se agrega comandos de ayuda y la verdad de valvo

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 var Discord = require('discord.js');
+const { createEmbedMessage, laVerdad, sendMentionMessage, sendNormalMessage, sendEmbedMessage } = require('./src/message');
+
 var bot = new Discord.Client();
+const attachment = new Discord.Attachment('./images/valvo-avatar.png', 'valvo-avatar.png');
 var auth = require('./auth.json');
 bot.login(auth.token);
 
@@ -8,61 +11,110 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (message) => {
-    if (message.content.startsWith('valvo')) {
-        var args = message.content.substring(6);
-        switch(args) {
+
+    if (message.channel.type == 'dm' && message.author.tag != bot.user.tag) {
+
+        if (message.content.startsWith('!')) {
+            let args = message.content.substring(1);
+            console.log(args);
+            switch (args) {
+                case 'comandos':
+                    let comandos = [{
+                            name: "prefijo",
+                            value: "valvo"
+                        },
+                        {
+                            name: "apañes",
+                            value: "`apañai?` `estoy aburrido` `eres un bot?`"
+                        },
+                        {
+                            name: "jugemos",
+                            value: "`fable` `warframe` `anthem` `piratas` `apb` `forza` `black desert`"
+                        }
+                    ]
+                    message.author.send(createEmbedMessage(undefined, comandos, attachment, undefined));
+                    break;
+            }
+        }
+    }
+
+    // frases de apañe
+    if (message.content.startsWith('valvo') && message.channel.type != 'dm') {
+        let args = message.content.substring(6);
+        switch (args) {
             case 'apañai?':
                 message.channel.send('demás po perro!');
-            break;
+                break;
             case 'estoy aburrido':
                 message.channel.send('que quiere jugar? yo apaño a lo que sea');
-            break;
-            case 'jugemos fable?':
-                message.channel.send('ya po! abriendo su fable');
-                bot.user.setActivity('Fable', { type: 'Playing' })
-                .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-                .catch(console.error);
-            break; 
-            case 'jugemos warframe?':
-                message.channel.send('uuuuuhh su framio, YPAPUY IY UTYKATJEP');
-                bot.user.setActivity('Warframe', { type: 'Playing' })
-                .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-                .catch(console.error);
-            break; 
-            case 'jugemos anthem?':
-                message.channel.send('no hay otra wea? little biiiiitch, ya igual si');
-                bot.user.setActivity('Anthem', { type: 'Playing' })
-                .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-                .catch(console.error);
-            break; 
-            case 'jugemos black desert?':
-                message.channel.send('perro aunque nunca me carrió, ahora es el momento para que lo haga');
-                bot.user.setActivity('Black Desert Online', { type: 'Playing' })
-                .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-                .catch(console.error);
-            break; 
-            case 'jugemos piratas?':
-                message.channel.send('siempre y cuando me den cofres');
-                bot.user.setActivity('Sea of Thieves', { type: 'Playing' })
-                .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-                .catch(console.error);
-            break; 
-            case 'jugemos forza?':
-                message.channel.send('perro nunca pensé escuchar eso, entrando')
-                bot.user.setActivity('Forza Horizon', { type: 'Playing' })
-                .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-                .catch(console.error);
-            break; 
-            case 'jugemos apb?':
-                message.channel.send('uuuuuuhh igual, me vestiré de carabibueno para la ocasión')
-                bot.user.setActivity('APB Reloaded', { type: 'Playing' })
-                .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-                .catch(console.error);
-            break;
+                break;
+            case 'ayuda':
+                message.author.send(`uuuh que pasa ${message.author.username}? a que quiere que le apañe?`);
+                break;
             case 'deja de apañar':
                 message.channel.send('lo siento, tenía muchas ganas de apañar');
                 bot.user.setActivity(null);
-            break;
-         }
+                break;
+            case 'eres un bot?':
+                let verdad = laVerdad();
+                sendMentionMessage(':incoming_envelope: acompañame, te contaré una triste historia', message);
+                message.author.send(createEmbedMessage(undefined, verdad, attachment, undefined));
+                break;
+        }
     }
+    // frases jugemos
+    if (message.content.startsWith('valvo jugemos')) {
+        let args = message.content.substring(14);
+        console.log(args);
+        switch (args) {
+            case 'fable?':
+                message.channel.send('ya po! abriendo su fable');
+                bot.user.setActivity('Fable', { type: 'Playing' })
+                    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+                    .catch(console.error);
+                break;
+            case 'warframe?':
+                message.channel.send('uuuuuhh su framio, YPAPUY IY UTYKATJEP');
+                bot.user.setActivity('Warframe', { type: 'Playing' })
+                    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+                    .catch(console.error);
+                break;
+            case 'anthem?':
+                message.channel.send('no hay otra wea? little biiiiitch, ya igual si');
+                bot.user.setActivity('Anthem', { type: 'Playing' })
+                    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+                    .catch(console.error);
+                break;
+            case 'black desert?':
+                message.channel.send('perro aunque nunca me carrió, ahora es el momento para que lo haga');
+                bot.user.setActivity('Black Desert Online', { type: 'Playing' })
+                    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+                    .catch(console.error);
+                break;
+            case 'piratas?':
+                message.channel.send('siempre y cuando me den cofres');
+                bot.user.setActivity('Sea of Thieves', { type: 'Playing' })
+                    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+                    .catch(console.error);
+                break;
+            case 'forza?':
+                message.channel.send('perro nunca pensé escuchar eso, entrando')
+                bot.user.setActivity('Forza Horizon', { type: 'Playing' })
+                    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+                    .catch(console.error);
+                break;
+            case 'apb?':
+                message.channel.send('uuuuuuhh igual, me vestiré de carabibueno para la ocasión')
+                bot.user.setActivity('APB Reloaded', { type: 'Playing' })
+                    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+                    .catch(console.error);
+                break;
+            default:
+                message.channel.send('uuuuuuh pero peeeerro no tengo ese gamesio, lo bajo al toque, pa puro apañar');
+                bot.user.setActivity(null);
+                break;
+        }
+
+    }
+
 });
---
+++ src/message.js
@@ -0,0 +1,52 @@
+const { RichEmbed } = require('discord.js');
+
+exports.createEmbedMessage = (author = undefined, fields = undefined, thumbNail = undefined, footer = undefined) => {
+    let embed = new RichEmbed();
+
+    if (undefined != author) {
+        embed.setAuthor(author);
+    }
+
+    if (fields.length > 0 && undefined != fields) {
+        for (const field of fields) {
+            embed.addField(field.name, " " + field.value)
+        }
+    }
+
+    if (undefined != thumbNail) {
+        embed.attachFile(thumbNail);
+        embed.setThumbnail('attachment://valvo-avatar.png');
+    }
+
+    if (undefined != footer) {
+        embed.setFooter(footer.text, footer.icon);
+    }
+    embed.setColor(0x860202);
+
+    return embed;
+}
+
+exports.sendMentionMessage = (text, message) => {
+    message.channel.send(`${message.author.toString()} ` + text);
+}
+
+exports.sendNormalMessage = (text, message) => {
+    message.channel.send(text);
+};
+
+exports.sendEmbedMessage = (embed, message) => {
+    message.channel.send(embed);
+}
+
+exports.laVerdad = () => {
+    let verdad = [{
+            name: "La verdad",
+            value: "la verdad es que soy un bot creado en el año 2020 para recordar los dias en los que valvo apañaba, la fuerte necesidad de esperar un yo te apaño de valvo le dio vida a este bot"
+        },
+        {
+            name: "ayuda",
+            value: "`!comandos`"
+        }
+    ]
+    return verdad;
+}
