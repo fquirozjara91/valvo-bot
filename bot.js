3e17ef958d0326141551438686d8efffd93b5845
Wigan <felipe.quirozj@hotmail.com>
2020-01-15 00:11:30 -0300

    se agrega worker

--- bot.js
+++ bot.js
@@ -1,17 +1,18 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-// Configure logger settings
+
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
+
 logger.level = 'debug';
-// Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
